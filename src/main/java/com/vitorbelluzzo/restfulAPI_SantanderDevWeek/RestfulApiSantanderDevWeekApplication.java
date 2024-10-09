92975eee5f39985f58ea45feaf9280adc81780af
vitorbelluzzo[ <vitorbelluzzo@hotmail.com>
2024-10-08 23:27:25 -0300

    initial commit

---
+++ src/main/java/com/vitorbelluzzo/restfulAPI_SantanderDevWeek/RestfulApiSantanderDevWeekApplication.java
@@ -0,0 +1,13 @@
+package com.vitorbelluzzo.restfulAPI_SantanderDevWeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiSantanderDevWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiSantanderDevWeekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitorbelluzzo/restfulAPI_SantanderDevWeek/RestfulApiSantanderDevWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorbelluzzo.restfulAPI_SantanderDevWeek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiSantanderDevWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
